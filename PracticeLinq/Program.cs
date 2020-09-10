cc52c407aea0d155f8c9e524558701704dce0404
Bryce <ImperialEagle1998@gmail.com>
2020-09-10 13:22:42 -0500

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videogames = new List<string>();
+            videogames.Add("Railroad Tycoon 3");
+            videogames.Add("GearCity");
+            videogames.Add("Europa Universalis IV");
+            videogames.Add("BeamNG Drive");
+            videogames.Add("Floating Simulator");
+
+
+            var gamesordered = videogames.OrderBy(game =>  game.Length);
+            Console.ReadLine();
+          
+
+        }
+    }
+}
